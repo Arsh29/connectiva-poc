571c754c3c7e88f5d22b36becafaeb7a1066667d
Arsh29 <35421434+Arsh29@users.noreply.github.com>
2023-12-25 00:12:41 +0530

    Feature/add login api (#3)
    * Update README.md
    
    * basic local jwt authentication without roles on mongodb
    
    * Create Connectiva.postman_collection.json
    
    * Update SecurityConfig.java

---
+++ poc/src/main/java/com/connectiva/poc/model/JwtRequest.java
@@ -0,0 +1,13 @@
+package com.connectiva.poc.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class JwtRequest {
+    private String email;
+    private String password;
+}
